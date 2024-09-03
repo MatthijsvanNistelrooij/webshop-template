acec948f9d03c67e7410f36c9feec9d03a76ca81
Matthijs <matthijs.vannistelrooij@gmail.com>
2024-09-03 12:33:54 +0200

    update succes page

--- app/(root)/success/page.jsx
+++ app/(root)/success/page.jsx
@@ -1,29 +1,8 @@
-"use client"
-import React, { useEffect } from "react"
-
-import { success } from "../../../public/assets/images"
-import { useStateContext } from "../../../context/StateContext"
-import { runFireworks } from "../../../lib/utils"
-import Image from "next/image"
-import { Navbar } from "../../../components"
+import React from 'react'
 
 const Success = () => {
-  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext()
-
-  useEffect(() => {
-    localStorage.clear()
-    setCartItems([])
-    setTotalPrice(0)
-    setTotalQuantities(0)
-    runFireworks()
-  }, [])
-
   return (
-    <>
-      <div className="success-wrapper">
-        <Image src={success} width={1450} height={550} alt="success image" />
-      </div>
-    </>
+    <div>Success</div>
   )
 }
 
